cc86c75a1a02cbe912c762cd8b45a859c8f8348a
Anatoliy <staheev.a.n@yandex.ru>
2023-03-04 14:02:41 +0600

    second commit

---
+++ seminar3/script.js
@@ -0,0 +1,6 @@
+alert("привет мир");
+let user_name = prompt("введите своё имя");
+alert(`здравствуй ${user_name}`);
+let num = prompt(`${user_name} введи число`);
+let sum = 158 + num;
+alert(`158 + ${num} = ${sum}`);
