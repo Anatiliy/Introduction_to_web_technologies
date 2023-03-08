61ebdcd9067a785188edb2f48df26fba1e6f3cd3
Anatoliy <staheev.a.n@yandex.ru>
2023-03-08 21:02:53 +0600

    final commit

--- seminar3/script.js
+++ seminar3/script.js
@@ -1,6 +1,26 @@
 alert("привет мир");
 let user_name = prompt("введите своё имя");
-alert(`здравствуй ${user_name}`);
-let num = prompt(`${user_name} введи число`);
+function welcome(){
+    alert(`здравствуй ${user_name}`);
+}
+welcome()
+let num = Number(prompt(`${user_name} введи число`));
 let sum = 158 + num;
-alert(`158 + ${num} = ${sum}`);
+
+function ask_again(){
+    let result = Number(prompt(`${user_name} сколько будет 158 + ${num} ?`, "введи число"));
+    confirm('вы уверены') ? alert("проверим?"): ask_again();
+    return result;
+}
+function check(){
+    return sum == ask_again();
+}
+
+switch(check()) {
+    case true:
+        alert(`точно, 158 + ${num} = ${sum}`)
+        break
+    case false:
+        alert(`неверно, 158 + ${num} = ${sum}`)
+        break
+}
